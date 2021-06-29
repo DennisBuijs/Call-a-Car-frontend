3fdf46a2f22de6bf4ba43ed22e0ca0efa97ba49b
Dennis Buijs <dennisdennisbuijs@gmail.com>
2021-06-29 11:07:39 +0200

    Add sonarcloud

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,14 +3,16 @@ import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './components/header/header.component';
 import {AuthenticatedWrapperComponent} from './components/authenticated-wrapper/authenticated-wrapper.component';
 import {RouterModule} from '@angular/router';
-import { UnauthenticatedWrapperComponent } from './components/unauthenticated-wrapper/unauthenticated-wrapper.component';
+import {UnauthenticatedWrapperComponent} from './components/unauthenticated-wrapper/unauthenticated-wrapper.component';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
     declarations: [HeaderComponent, AuthenticatedWrapperComponent, UnauthenticatedWrapperComponent],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        HttpClientModule
     ]
 })
 export class CoreModule {
