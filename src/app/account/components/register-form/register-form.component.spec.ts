7457288d3349d1b81fa8b1e24f5bfe4b0a58cbff
Dennis Buijs <dennisdennisbuijs@gmail.com>
2021-07-08 19:25:29 +0200

    Add more unit tests

--- src/app/account/components/register-form/register-form.component.spec.ts
+++ src/app/account/components/register-form/register-form.component.spec.ts
@@ -2,9 +2,15 @@ import {async, TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {RegisterFormComponent} from './register-form.component';
+import {AuthService} from '../../../core/services/auth.service';
 
 describe('RegisterFormComponent', () => {
+
+    let authServiceSpy: jasmine.SpyObj<AuthService>;
+
     beforeEach(async(() => {
+        const registerSpy = jasmine.createSpyObj('AuthService', ['register']);
+
         TestBed.configureTestingModule({
             imports: [
                 RouterTestingModule,
@@ -13,7 +19,15 @@ describe('RegisterFormComponent', () => {
             declarations: [
                 RegisterFormComponent
             ],
+            providers: [
+                {
+                    provide: AuthService,
+                    useValue: registerSpy
+                }
+            ]
         }).compileComponents();
+
+        authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
     }));
 
     it(`should evaluate to valid`, () => {
@@ -44,5 +58,33 @@ describe('RegisterFormComponent', () => {
         const isPasswordValid = component.isPasswordValid();
 
         expect(isPasswordValid).toBeFalse();
+    });
+
+    it(`should not call 'register'`, () => {
+        const fixture = TestBed.createComponent(RegisterFormComponent);
+        const component = fixture.componentInstance;
+
+        component.login = {
+            username: '',
+            password: 'wachtwoord1234',
+            passwordCheck: 'wachtwoord0000'
+        };
+
+        expect(authServiceSpy.register).not.toHaveBeenCalled();
+    });
+
+    it(`should call 'register'`, () => {
+        const fixture = TestBed.createComponent(RegisterFormComponent);
+        const component = fixture.componentInstance;
+
+        component.login = {
+            username: '',
+            password: 'wachtwoord1234',
+            passwordCheck: 'wachtwoord1234'
+        };
+
+        component.onSubmit();
+
+        expect(authServiceSpy.register).toHaveBeenCalled();
     });
 });
